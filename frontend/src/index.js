3a2c9faa918a09c3fdd8306ed0eed1bcee4fdeb8
Messam Ali <messam.ali@virtual-force.com>
2026-03-13 10:12:44 +0500

    changed frontend API to localhost from Prod VMs IP

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ function add() {
     const n1 = document.getElementById("num1").value;
     const n2 = document.getElementById("num2").value;
 
-    fetch(`http://172.28.5.11:5000/add?a=${n1}&b=${n2}`)
+    fetch(`http://localhost:5000/add?a=${n1}&b=${n2}`)
         .then(res => res.text())
         .then(data => {
             document.getElementById("result").innerText = "Result: " + data;
@@ -16,7 +16,7 @@ function multiply() {
     const n1 = document.getElementById("num1").value;
     const n2 = document.getElementById("num2").value;
 
-    fetch(`http://172.28.5.11:5000/multiply?a=${n1}&b=${n2}`)
+    fetch(`http://localhost:5000/multiply?a=${n1}&b=${n2}`)
         .then(res => res.text())
         .then(data => {
             document.getElementById("result").innerText = "Result: " + data;
